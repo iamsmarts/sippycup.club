6bee48ad25d914adaa3cacfdff7e7391b688aafd
Rudy Quevedo <rudy@iamsmarts.com>
2015-04-01 11:51:38 -0700

    sippycupclub initial commit
    initial commit  for sippycup.club yeoman angular setup.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sippycupclubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sippycupclubApp
+ */
+angular.module('sippycupclubApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sippycupclubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sippycupclubApp
+ */
+angular.module('sippycupclubApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
